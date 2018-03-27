c97f007771ceb769ce7d8138286ff716ce957308
Ryan Choi <ryan.mingyu.choi@gmail.com>
2018-03-27 07:44:32 -0700

    OGs

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -6,7 +6,7 @@
 <meta property="og:title" content="Jemma & Ryan Wedding">
 <meta property="og:description" content="Be Our Guest<br>06.02.2018 3:45PM">
 <meta property="og:image" content="https://jemmaryan.com/img/invitation.jpg">
-<meta property="og:url" content="https://jemmaryan.com">
+<meta property="og:url" content="jemmaryan.com">
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <link rel="icon" href="/img/favicon.ico">
 
