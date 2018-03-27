0ab1b6af9d5398728f29fee58960697be7be4b48
Ryan Choi <ryan.mingyu.choi@gmail.com>
2018-03-27 07:47:23 -0700

    OGs

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -4,8 +4,8 @@
 <meta name="keywords" content="">
 <meta name="author" content="Ryan Mingyu Choi">
 <meta property="og:title" content="Jemma & Ryan Wedding">
-<meta property="og:description" content="Be Our Guest<br>06.02.2018 3:45PM">
-<meta property="og:image" content="https://jemmaryan.com/img/invitation.jpg">
+<meta property="og:description" content="06.02.2018 3:45PM">
+<meta property="og:image" content="https://jemmaryan.com/img/og-invitation.jpg">
 <meta property="og:url" content="jemmaryan.com">
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <link rel="icon" href="/img/favicon.ico">
